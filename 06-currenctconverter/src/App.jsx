73f7cadf4c0c73723ed5d740a552ac3159b810c0
PREM015 <rprem3096@gamil.com>
2025-03-29 19:44:53 +0530

    push 8

---
+++ 06-currenctconverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-6xl font-mono'>hello</h1>
+    </>
+  )
+}
+
+export default App

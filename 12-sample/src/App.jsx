372a193c51a08e7d239b8504a2c41e40eabd9f00
PREM015 <kumarrahuljmp8@gmail.com>
2025-04-17 18:51:14 +0530

    temp

---
+++ 12-sample/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.VITE_APP_WRITE__URL)
+  return (
+    <>
+
+      <h1>a blog with appwrite</h1>
+    </>
+  )
+}
+
+export default App
